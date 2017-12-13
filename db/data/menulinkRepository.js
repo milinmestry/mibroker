423a707bbc6c0293544c29844a4fd252165ab2c0
Milin Mestry <milinmestry@gmail.com>
2017-12-13 15:43:17 +0530

    Footer menu links display using database values.

--- db/data/menulinkRepository.js
+++ db/data/menulinkRepository.js
@@ -1,14 +1,18 @@
 const db = require('../../models');
 
+const Op = db.Sequelize.Op;
+
 const menulinkRepository = {
   getTopMenus: () => {
     return db.menulink.findAll({
       attributes: [
-        'label', 'slug', 'menu_title', 'is_external',
+        'label', 'slug', 'menu_title', 'is_external', 'menu_position',
       ],
       where: {
         is_active: 1,
-        menu_position: 'TOP',
+        menu_position: {
+          [Op.or]: ['TOP', 'BOTTOM'],
+        },
       },
     }).then(function (links) {
       return links;
