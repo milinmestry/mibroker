9ca5a647a7b92c18e480e756a7c65d99ca9f2322
Milin Mestry <milinmestry@gmail.com>
2018-02-25 16:30:47 +0530

    Added common directory into git @home

---
+++ common/constant-vars.js
@@ -0,0 +1,15 @@
+exports.USER_STATUS = {
+  'ACTIVE': 'active',
+  'INACTIVE': 'inactive',
+  'REGISTERED': 'registered',
+  'SUSPENDED': 'suspended',
+};
+
+exports.ACCOUNT_LOCKED = {
+  'YES': 'yes',
+  'NO': 'no',
+};
+
+exports.SITE_INFO = {
+  'USERNAME': 'System',
+};
